9c547e0654c213678d3183183fa3d0a21dbd316a
johntusc <johnttra@usc.edu>
2025-10-05 23:00:55 -0700

    cleaned csv, completed sql_where and sql_join

--- sql.py
+++ sql.py
@@ -1,3 +1,5 @@
+import re 
+
 class SQL:
     def __init__(self):
         ''' 
@@ -6,15 +8,16 @@ class SQL:
         '''
         self.database = {}
     
-    def add_table(self, file, name):
+    def add_table(self, file, name, sep): # DONE
         '''
+        Parsing function
         Parse the csv file and store it as a dictionary.
         '''
         with open(file, 'r') as f:
             f_lines = f.readlines()
 
             for i in range(len(f_lines)):
-                line = f_lines[i].strip().split(',')
+                line = f_lines[i].strip().split(sep)
                 
                 if i == 0:
                     data_dict = {key:[] for key in line}
@@ -24,5 +27,90 @@ class SQL:
                         data_dict[key].append(val)
             
         self.database[name] = data_dict
-    
-    
+
+    def sql_select():
+        pass
+
+    def sql_from(self, command): # DONE
+        try:
+            name = command.split(' ')[1]
+            table = self.database[name]
+            return table
+        except:
+            print('Query error: FROM ...')
+            return False
+
+    def sql_join(self, command, table): # DONE
+        '''
+        Sample query: JOIN t2 ON t1.a = t2.b
+        '''
+        error = 'Query error: JOIN ... ON ...'
+        try:
+            name = command.split(' ')[1].strip()
+
+            on = command.split('ON')[1]
+
+            left = on.split('=')[0].split('.')
+            right = on.split('=')[1].split('.')
+
+            name1 = left[0].strip()
+            name2 = right[0].strip()
+
+            t1, key1 = self.database[name1], left[1].strip()
+
+            t2, key2 = self.database[name2], right[1].strip()
+
+            if self.database[name] != t2:
+                print(error)
+                print('Table in JOIN clause does not match with table in ON clause.')
+                return False
+            elif table != t1:
+                print(error)
+                print('Table in FROM clause does not match with table in ON clause.')
+                return False
+
+            common_elems = [item for item in t1[key1] if item in t2[key2]]
+
+            indices1 = [t1[key1].index(elem) for elem in common_elems]
+            indices2 = [t2[key2].index(elem) for elem in common_elems]
+
+            t1 = {f'{name1}.{key}':[val_list[i] for i in indices1] for key, val_list in t1.items()}
+            t2 = {f'{name2}.{key}':[val_list[i] for i in indices2] for key, val_list in t2.items()}
+
+            table = {**t1, **t2}
+            return table
+
+        except:
+            print(error)
+            return False
+
+    def sql_groupby():
+        pass
+
+    def sql_where(self, table, conds):
+        command = []
+        # Pattern for not, and, or: (\bNOT\b|\bAND\b|\bOR\b)
+        # Pattern for parentheses: (\(|\))
+        # Pattern for simple conditions: (\w+\s*=\s*'[^']*')
+        pattern = r"(\bNOT\b|\bAND\b|\bOR\b)|(\(|\))|(\w+\s*=\s*'[^']*')"
+        matches = re.findall(pattern, conds, re.IGNORECASE)
+        conds = [t for match in matches for t in match if t]
+
+        for cond in conds:
+            if (cond == 'NOT') or (cond == 'not'):
+                command.append('~')
+            elif '(' in cond:
+                command.append(cond['('])
+            elif ')' in cond:
+                command.append(cond[')'])
+            elif (cond == 'AND') or (cond == 'and'):
+                command.append('&')
+            elif (cond == 'OR') or (cond == 'or'):
+                command.append('|')
+            else:
+                pass
+
+    def query(string):
+        clauses = ['SELECT', 'FROM', 'JOIN', 'ON', 'WHERE', 'GROUP BY']
+        commands = []
+        pass
